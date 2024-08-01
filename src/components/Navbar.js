91d8c714dbb6c33c39036e5eceac12c36975c5c5
Allivia Jaazielle <llivicaina@gmail.com>
2024-08-02 00:47:20 +0800

    new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,9 @@
-import React, { useEffect } from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
-import '../styles/Navbar.css'
-import ReorderIcon from '@material-ui/icons/Reorder'
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import '../styles/Navbar.css';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false);
-  const location = useLocation();
-
-  useEffect(() => {
-    setExpandNavbar(false);
-  }, [location]);
 
   const downloadResume = () => {
     const link = document.createElement('a');
@@ -21,23 +14,27 @@ function Navbar() {
     document.body.removeChild(link);
   };
 
-
   return (
     <div className="navbar" id={expandNavbar ? "open" : "close"}>
       <h2>Via Cai</h2>
       <div className="toggleButton">
-        <button onClick={() => {
-          setExpandNavbar(prev => !prev);
-        }}>
+        <button onClick={() => setExpandNavbar(prev => !prev)}>
           <ReorderIcon />
         </button>
       </div>
-    
       <div className='links'>
-        <NavLink to="/"  activeClassName="active-link">  <span style={{ color: 'white' }}>01.</span>Home </NavLink>
-        <NavLink to="/projects" activeClassName="active-link">  <span style={{ color: 'white' }}>02.</span>Projects </NavLink>
-        <NavLink to="/certifications" activeClassName="active-link">  <span style={{ color: 'white' }}>03.</span>Certifications </NavLink>
-        <NavLink to="/contact"  activeClassName="active-link"><span style={{ color: 'white' }}>04.</span>Contacts </NavLink>
+        <a href="#home" className="nav-link">
+          <span style={{ color: 'white' }}>01.</span> Home
+        </a>
+        <a href="#projects" className="nav-link">
+          <span style={{ color: 'white' }}>02.</span> Projects
+        </a>
+        <a href="#certifications" className="nav-link">
+          <span style={{ color: 'white' }}>03.</span> Certifications
+        </a>
+        <a href="#contact" className="nav-link">
+          <span style={{ color: 'white' }}>04.</span> Contact
+        </a>
         <button onClick={downloadResume} className="resume-button">DOWNLOAD RESUME</button>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,140 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import EmailIcon from '@material-ui/icons/Email';
+import Navbar from '../components/Navbar'; // Adjust this path if necessary
 import "../styles/Home.css";
+import "../styles/Projects.css";
+import "../styles/certs.css";
+import "../styles/divider.css";
 
-function Home() {
+function CombinedPage() {
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+  const [lightboxImage, setLightboxImage] = useState('');
+
+  const openLightbox = (imgSrc) => {
+    setLightboxImage(imgSrc);
+    setIsLightboxOpen(true);
+  };
+
+  const closeLightbox = () => {
+    setIsLightboxOpen(false);
+  };
+
   return (
-    <div className='home'>
-      <div className='about'>
-        <img src = '/prof3.jpg' alt='Profile' className='profile-picture'/>
-        <h3>Hi, my name is</h3>
-        <h2>Allivia Jaazielle Caiña.</h2>
-        <div className='prompt'>
-          <p>
-            A Computer Engineering graduate from Adamson University. I am currently
-            focusing on Web Development and Data Analytics using Excel, Python, SQL, and Power BI.
-          </p>
+    <div className='combined-page'>
+      <div id="home" className='home'>
+        <div className='about'>
+          <img src='/prof2.png' alt='Profile' className='profile-picture' />
+          <h3>Hi, my name is</h3>
+          <h2>Allivia Jaazielle Caiña.</h2>
+          <div className='prompt'>
+            <p>
+              A <b>Computer Engineering</b> graduate from Adamson University. I am currently
+              focusing on Web Development and Data Analytics using Excel, Python, SQL, NoSQL and Power BI.
+            </p>
+          </div>
+          <div className='logos'>
+            <img src='/logos/excel.png' alt='Excel Logo' />
+            <img src='/logos/python.png' alt='Python Logo' />
+            <img src='/logos/sql.png' alt='SQL Logo' />
+            <img src='/logos/powerbi.png' alt='Power BI Logo' />
+            <img src='/logos/github.png' alt='GitHub Logo' />
+            <img src='/logos/mongo.png' alt='Mongo Logo' />
+          </div>
         </div>
-        <div className='logos'>
-          <img src='/logos/excel.png' alt='Excel Logo' />
-          <img src='/logos/python.png' alt='Python Logo' />
-          <img src='/logos/sql.png' alt='SQL Logo' />
-          <img src='/logos/powerbi.png' alt='Power BI Logo' />
-          <img src='/logos/github.png' alt='GitHub Logo' />
+      </div>
+      <div class="divider-wrapper">
+          <img src="/divider1.png" alt="Divider" class="divider" />
+      </div>
+
+      <div id="projects" className='portfolio'>
+        <h1>My Projects</h1>
+        <p>These projects showcase my expertise in handling data through meticulous processes of cleaning, wrangling, analysis, and visualization.</p>
+        <div className='cards-container'>
+          <div className='card'>
+            <a href='#' className='card-link'>
+              <div className='card-content'>
+                <h2>Plant Corporation Quantity Performance 2023</h2>
+                <p>Description of project 1.</p>
+              </div>
+            </a>
+          </div>
+          <div className='card'>
+            <a href='#' className='card-link'>
+              <div className='card-content'>
+                <h2>Two-Week Temperature Analysis and Data Visualization in Pasay City</h2>
+                <p>Description of project 2.</p>
+              </div>
+            </a>
+          </div>
+          <div className='card'>
+            <a href='#' className='card-link'>
+              <div className='card-content'>
+                <h2>Project 3</h2>
+                <p>Description of project 3.</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="divider-wrapper">
+          <img src="/divider2.png" alt="Divider" class="divider" />
+      </div>
+      <div id="certifications" className='certs'>
+        <h1>My Certifications</h1>
+        <div className="image-container">
+          <img
+            id="cert1"
+            src="cert1.jpg"
+            alt="Descriptive Alt Text"
+            onClick={() => openLightbox('cert1.jpg')}
+          />
+          <img
+            id="cert2"
+            src="cert2.jpg"
+            alt="Descriptive Alt Text"
+            onClick={() => openLightbox('cert2.jpg')}
+          />
+          <img
+            id="cert1"
+            src="cert1.jpg"
+            alt="Descriptive Alt Text"
+            onClick={() => openLightbox('cert1.jpg')}
+          />
+        </div>
+        {isLightboxOpen && (
+          <div className="lightbox" onClick={closeLightbox}>
+            <span className="close" onClick={closeLightbox}>&times;</span>
+            <img className="lightbox-content" src={lightboxImage} alt="Zoomed Image" />
+          </div>
+        )}
+      </div>
+      <div class="divider-wrapper">
+          <img src="/divider3.png" alt="Divider" class="divider" />
+      </div>
+      <div id="contact" className='box'>
+        <div className='card'>
+          <h1>Contact Me!</h1>
+          <div className="form-group">
+            <label htmlFor="FN">Full Name:</label>
+            <input type="text" name="FN" placeholder="Full Name"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" placeholder="Email"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea name="comment" form="usrform" placeholder="Enter text here..."></textarea>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Get in touch" className="get-in-touch"/>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default Home;
+export default CombinedPage;
