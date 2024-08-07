1c00e565f4d39b9e93a6dca87959768d7f457171
Allivia Jaazielle <llivicaina@gmail.com>
2024-08-07 21:46:16 +0800

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,11 +9,11 @@ function Footer() {
   return (
     <div className='footer'>
         <div className='socialMedia'>
-            <a href = "https://www.linkedin.com/" target="_blank"><LinkedInIcon /></a>
+            <a href = "https://www.linkedin.com/in/allivia-caina/" target="_blank"><LinkedInIcon /></a>
             <a href = "https://github.com/Via-Cai" target="_blank"><GitHubIcon /></a>
-            <a href = "mailto:llivicaina@gmail.com" target="_blank"><EmailIcon /></a>
+            <a href = "mailto:via.caina@gmail.com" target="_blank"><EmailIcon /></a>
         </div>
-        <p>Allivia Jaazielle Caina <br></br>09764774893 | via.caina@gmail.com <br></br> &copy; 2024 vercel.com </p>
+        <p>Allivia Jaazielle Caiña <br></br>09764774893 | via.caina@gmail.com <br></br> &copy; 2024 vercel.com </p>
     </div>
   )
 }
