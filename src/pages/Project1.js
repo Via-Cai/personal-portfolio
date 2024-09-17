eeaeeea00e2becb0ca0074fc524515702ec0511c
Via Caina <via.caina@gmail.com>
2024-09-18 02:35:13 +0800

    update

--- src/pages/Project1.js
+++ src/pages/Project1.js
@@ -6,10 +6,11 @@ function Project1() {
     <div className='project1'>
       <div className='space'></div>
       <div className='pictures'>
-        <img src="proj1_1.png" alt="Homepage"/>
+        <img src="proj1.png" alt="Homepage"/>
+        <img src="HomePage.png" alt="Powerbi"/>
         <img src="proj1_2.png" alt="Powerbi"/>
         <img src="sales.png" alt="Powerbi"/>
-        <img src="HomePage.png" alt="Powerbi"/>
+     
       </div>
     </div>
   )
